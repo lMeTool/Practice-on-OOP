979492f3e3dfe8f336c03196448a5257b2c0aa45
Zenkin Danil <co0kisszda@gmail.com>
2025-06-27 20:32:34 +0800

    completed example1

--- Example1.py
+++ Example1.py
@@ -3,8 +3,9 @@ class Product:
     # Инициализатор должен принять на вход
     # название (name) и количество (quantity) товара.
     # В теле инициализатора задайте соответствующие атрибуты экземпляра класса.
-    def __init__(...):
-        ...
+    def __init__(self, name: str, quantity: int):
+        self.name = name
+        self.quantity = quantity
 
     def get_basic_info(self):
         return f'{self.name} (в наличии: {self.quantity})'
@@ -15,15 +16,26 @@ class Product:
 
 class Kettlebell(Product):
     # Опишите инициализатор класса и переопределите метод get_full_info()
-    ...
+    def __init__(self, name, quantity, weight):
+        super().__init__(name, quantity)
+        self.weight = weight
+
+    def get_full_info(self):
+        return self.get_basic_info() + f'. Вес: {self.weight} кг'
 
 
 class Clothing(Product):
     # Опишите инициализатор класса и переопределите метод get_full_info()
-    ...
+    def __init__(self, name, quantity, size):
+        super().__init__(name, quantity)
+        self.size = size
 
+    def get_full_info(self):
+        return self.get_basic_info() + f'. Размер: {self.size}'
 
 # Для проверки вашего кода создадим пару объектов...
+
+
 small_kettlebell = Kettlebell('Гиря малая', 15, 2)
 shirt = Clothing('Футболка', 5, 'L')
 
